0eb4ec41c9533249857f00e92f2ebc755796b30b
Kofi Nyarko Kumi <kofinyarkokumi@gmail.com>
2023-03-13 23:55:48 +0000

    modify app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchDataService } from './fetch-data.service';
+import { Product } from './types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'udacity-new-store';
+  products:Product[]=[];
+  cart:Product[]=[];
+
+  constructor(private fetchData: FetchDataService){
+
+  }
+
+  ngOnInit(): void {
+    this.fetchData.getData()
+      .subscribe(res => this.products = res);
+  }
+
+  addCart(product: Product):void{
+    this.cart.push(product);
+  }
+
+  countItems():number{
+    return this.cart.length;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductDisplayComponent } from './product-display/product-display.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CartlistComponent } from './cartlist/cartlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductDisplayComponent,
+    CartlistComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
