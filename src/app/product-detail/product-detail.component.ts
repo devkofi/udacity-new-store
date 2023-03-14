1e8d2b930f1bc588da05b6dee19a97ca87975003
Kofi Nyarko Kumi <kofinyarkokumi@gmail.com>
2023-03-14 16:23:43 +0000

    add component product-detail

---
+++ src/app/product-detail/product-detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { GetCurrentProductService } from '../get-current-product.service';
+import { Product } from '../types';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit{
+  product: Product;
+
+  @Output() addToCart = new EventEmitter();
+
+  constructor(private getProduct: GetCurrentProductService){
+
+  }
+
+  ngOnInit(): void {
+    this.product = this.getProduct.getCurrentProduct()
+  }
+}
